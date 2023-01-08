9c7a1bdb228229b39fd578473413592f4815fdd1
Ahammed Fahim Galib <galib0124gmail.com>
2023-01-08 17:49:34 +0600

    primary mongodb setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,15 @@ const port = process.env.PORT || 5000;
 // Middleware use 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.w7wfspi.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 
 app.get('/', (req, res) => {
